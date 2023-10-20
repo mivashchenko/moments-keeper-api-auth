f41afa03cca01c154c304c3651ba18fe3ba029a7
maksymivashchenko <Maksym.Ivashchenko@andorhealth.com>
2023-10-19 20:04:19 -0400

    add cors, change redirect URI

--- src/clients/server.client.ts
+++ src/clients/server.client.ts
@@ -1,43 +1,45 @@
-import express, { Express } from 'express';
+import express, {Express} from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import authRouter from '../routes/auth.router';
 
 class ServerApi {
-  private readonly app: Express;
+    private readonly app: Express;
 
-  private port = process.env.PORT || 3344;
+    private port = process.env.PORT || 3344;
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  private server: any;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    private server: any;
 
-  constructor() {
-    this.app = express();
-    this.app.use(bodyParser.json());
-    this.app.use(bodyParser.urlencoded({ extended: true }));
-    this.app.use('/', authRouter.getRouter());
-    this.server = undefined;
-  }
-
-  /**
-   * @returns void
-   */
-  public start(): void {
-    this.server = this.app.listen(this.port, () => {
-      console.log(`⚡️[server]: Server is running at port: ${this.port}`);
-    });
-  }
-
-  /**
-   * @returns void
-   */
-  public stop(): void {
-    if (this.server) {
-      this.server.close();
-      console.log('stopped');
-    } else {
-      console.log('not stopped');
+    constructor() {
+        this.app = express();
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({extended: true}));
+        this.app.use(cors());
+        this.app.use('/', authRouter.getRouter());
+        this.server = undefined;
     }
-  }
+
+    /**
+     * @returns void
+     */
+    public start(): void {
+        this.server = this.app.listen(this.port, () => {
+            console.log(`⚡️[server]: Server is running at portt: ${this.port}`);
+        });
+    }
+
+    /**
+     * @returns void
+     */
+    public stop(): void {
+        if (this.server) {
+            this.server.close();
+            console.log('stopped');
+        } else {
+            console.log('not stopped');
+        }
+    }
 }
 
 export default new ServerApi();
